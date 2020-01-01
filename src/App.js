8a58c98b47501310a883a25ea16dc1bea6be46c5
unknown <yunhunjun007@naver.com>
2020-01-01 23:13:33 +0900

    apply eslint, fix react setting error

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      작동하는지 봅시다~
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // const e = React.createElement;
 
